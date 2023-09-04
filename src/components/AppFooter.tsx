6b9982dddd3713d8075ca529f03d8c5d4ff2dd6b
FR 4490 <Fernando_Rivas@claro.com.do>
2023-09-04 19:19:59 -0400

    Changed footer.

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -4,8 +4,7 @@ function AppFooter() {
   const navigate = useNavigate();
 
   return <footer className="my-3 navbar navbar-expand navbar-light static-bottom text-center">
-    <span>Pensum Extractor DO</span>
-    <span>Fernando Rivas, 2021</span>
+    <span>Suany Rosario, Fernando Rivas, 2023</span>
     <span>
       <a
         href="http://scydact.github.io/"
@@ -15,7 +14,7 @@ function AppFooter() {
       </a>
       <span style={{ margin: '0 .5rem' }}>
         (<a
-          href="https://github.com/Scydact/pensum-extractor-react"
+          href="https://github.com/Scydact/pensum-suany"
           target="_blank"
           rel="noreferrer">
           Ver en GitHub
@@ -26,7 +25,7 @@ function AppFooter() {
     <span>
       En caso de problemas o sugerencias,
       favor escribir a <a
-        href="mailto:scydact@gmail.com?subject=[Pensum Extractor]">
+        href="mailto:suanyelizabeth@gmail.com?subject=[Pensum UASD]">
         scydact@gmail.com</a>
       <span
        className="muted" 
